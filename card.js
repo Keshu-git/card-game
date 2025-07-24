64c40e4060bd43aceed66d982f3f608c27ed43c3
Keshu-git <keshavrdhini2008@gmail.com>
2025-07-24 13:56:32 +0530

    initial commit

---
+++ card.js
@@ -0,0 +1,65 @@
+
+const icons = ['fa-dog', 'fa-cat', 'fa-fish', 'fa-frog', 'fa-spider', 'fa-hippo'];
+let flippedCards = [];
+
+function shuffle(array) {
+    return array.sort(() => 0.5 - Math.random());
+}
+
+function initializeGame() {
+    const cardGrid = document.getElementById('cardGrid');
+    cardGrid.innerHTML = '';
+    flippedCards = [];
+
+    const pairs = [...icons, ...icons];
+    const shuffledIcons = shuffle(pairs);
+
+    shuffledIcons.forEach(icon => {
+        const card = document.createElement('div');
+        card.classList.add('flip-card');
+        card.dataset.icon = icon;
+
+        card.innerHTML = `
+        <div class="flip-card-inner">
+          <div class="flip-card-front"></div>
+          <div class="flip-card-back"><i class="fas ${icon}"></i></div>
+        </div>
+      `;
+
+        card.addEventListener('click', () => flipCard(card));
+        cardGrid.appendChild(card);
+    });
+}
+
+function flipCard(card) {
+    const inner = card.querySelector('.flip-card-inner');
+
+    if (flippedCards.length === 2 || inner.classList.contains('flipped')) return;
+
+    inner.classList.add('flipped');
+    flippedCards.push(card);
+
+    if (flippedCards.length === 2) {
+        const [card1, card2] = flippedCards;
+        const val1 = card1.dataset.icon;
+        const val2 = card2.dataset.icon;
+
+        if (val1 === val2) {
+
+            setTimeout(() => {
+                card1.classList.add('hide');
+                card2.classList.add('hide');
+                flippedCards = [];
+            }, 700);
+        } else {
+
+            setTimeout(() => {
+                card1.querySelector('.flip-card-inner').classList.remove('flipped');
+                card2.querySelector('.flip-card-inner').classList.remove('flipped');
+                flippedCards = [];
+            }, 900);
+        }
+    }
+}
+
+initializeGame();
